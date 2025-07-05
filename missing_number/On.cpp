3dfb9ba50a7cb0419f66373925fd02eeb3e3c235
MohakGupta2004 <mohakgupta500@gmail.com>
2025-07-05 16:00:08 +0530

    new optimization

--- missing_number/On.cpp
+++ missing_number/On.cpp
@@ -7,13 +7,13 @@ using namespace std;
 int main() {
     long long int n;
     cin >> n;
-    vector<long long int> arr(n-1);
+    long long int input;
     long long int missing_num=-1;
     long long int total = 0;
     long long int count = 0;
     for(long long int i = 0; i < n-1; i++) {
-        cin >> arr[i];
-        total+=arr[i];
+        cin >> input;
+        total+=input;
         count+=i+1;
     }
     count+=n;
